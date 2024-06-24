97f318ceaef292ed66553ddf72ca31c09e248925
Richard <57541025+richardh05@users.noreply.github.com>
2024-06-24 22:40:53 +0100

    Initialisation

---
+++ .config/fish/config.fish
@@ -0,0 +1,18 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set fish_greeting
+#set -U fish_greeting "🐟"
+
+# == ABBREVIATIONS ==
+abbr vi "nvim"
+abbr vim "nvim"
+abbr la "ls -a"
+abbr s "sudo"
+abbr p "sudo pacman"
+abbr spt "spotify_player"
+abbr stowdots "stow -d ~/dots/home -t ~/ ."
+abbr i3conf "nvim ~/.config/i3/config"
+abbr kittyconf "nvim .config/kitty/kitty.conf"
+abbr fishconf "nvim .config/fish/config.fish"
