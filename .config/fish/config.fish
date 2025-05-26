fa2a16614c755b258e93a61ae8b7592f6c90f48e
Richard <57541025+richardh05@users.noreply.github.com>
2025-05-26 22:55:15 +0100

    cleaning my /home

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,9 +13,11 @@ abbr s "sudo"
 abbr p "sudo pacman"
 abbr spt "spotify_player"
 #abbr stowdots "stow -d ~/dots/home -t ~/ ."
+abbr neofetch "fastfetch"
 abbr i3conf "nvim ~/.config/i3/config"
 abbr kittyconf "nvim .config/kitty/kitty.conf"
 abbr fishconf "nvim .config/fish/config.fish"
+set -gx PATH "$HOME/Scripts:$HOME/.local/share/Jetbrains/Toolbox/scripts:$HOME/.local/share/dotnet:$PATH"
 
 #wants to be at the end
 zoxide init fish | source
